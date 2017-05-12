e277853bf361dc211cacd196396d2dccc85bf184
shunyamm <gnjwth@gmail.com>
2017-05-12 15:13:12 +0900

    change files

--- foodapp.py
+++ foodapp.py
@@ -7,21 +7,19 @@ from datetime import datetime
 print ("test2 works!")
 
 app = Flask(__name__)
-html = urllib.request.urlopen('https://r.gnavi.co.jp/gant402/review/')
-
-#coding: utf-8
-
+html = urllib.request.urlopen("https://r.gnavi.co.jp/gant402/review/").read()
 
 #変数htmlには上記のHTMLがstrで代入されているとします。
 soup = BeautifulSoup(html,"lxml")
 
-#変数title, timestamp, author, author_link, bodyにそれぞれタイトル、投稿日時、著者、著者のリンク、記事本文が代入されます。
-title = soup.find('p',class_='trip-advisor-review-day--post')
-
+#変数titleにタイトルが代入されます。
+main_body = soup.find("div", {"class": "unit-inner"})
+reviews = main_body.find("ul", class_= "trip-review")
+titles = main_body.find("li",{"class": "trip-advisor-review__list.cx"})
 
 @app.route('/')
 def index():
-    return render_template('page0.html', message="こんにちは",title=title)
+    return render_template('page0.html', message="こんにちは",title=main_body)
 
 @app.route("/page1")
 def page1():
