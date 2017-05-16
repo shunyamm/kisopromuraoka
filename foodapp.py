b4a0589ac41e9afc9b67d0aa808411cb31709b1f
shunyamm <gnjwth@gmail.com>
2017-05-16 11:52:21 +0900

    add new files

--- foodapp.py
+++ foodapp.py
@@ -13,17 +13,36 @@ html = urllib.request.urlopen("https://r.gnavi.co.jp/gant402/review/").read()
 soup = BeautifulSoup(html,"lxml")
 
 #変数titleにタイトルが代入されます。
-main_body = soup.find("div", {"class": "unit-inner"})
-reviews = main_body.find("ul", class_= "trip-review")
-titles = main_body.find("li",{"class": "trip-advisor-review__list.cx"})
+main_body = soup.find("div", {"id": "main"})
+
 
 @app.route('/')
-def index():
-    return render_template('page0.html', message="こんにちは",title=main_body)
+def index2():
+    return render_template('index.html', message="こんにちは",review=main_body)
 
-@app.route("/page1")
-def page1():
-    return render_template('page1.html', user="ゲスト")
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+@app.route("/signin")
+def signin():
+    return render_template('signin.html')
+
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
+
+@app.route("/sidebar-left")
+def sidebarleft():
+    return render_template('sidebar-left.html')
+
+@app.route("/sidebar-right")
+def sidebarright():
+    return render_template('sidebar-right.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
